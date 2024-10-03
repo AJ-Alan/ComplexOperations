f3cf1a0962e94907ae3541fee3a249b8eb73f2e0
AJ <124542692+AJ-Alan@users.noreply.github.com>
2024-10-03 19:06:03 +0800

    Update ComplexOperations.py

--- ComplexOperations.py
+++ ComplexOperations.py
@@ -1,7 +1,7 @@
 from mpmath import * #for special math functions to be expanded in the future
-import numpy as np #for numpy arrays
+import numpy as np #for numpy arrays/list 
 
-def complexString(a):
+def complexString(a): #function that prints a string based on the complex number list
   if a[1] == 0: #pure reals
     return str(a[0]) #only returns real numbers
   elif a[0] == 0: #pure imaginary
@@ -17,4 +17,17 @@ def addC(c1,c2): #to add
 def subtractC(c1,c2): #to subtract
   return c1 - c2
 
+def quotient(c1,c2): #to divide -> c1 is numerator and c2 is denominator
+  if c2[0] == 0 and c2[1] == 0:
+    return "Complex infinity"
+  else:
+    divide = c2[0]**2 + c2[1]**2 #divisor for both real and imaginary parts
+    real = (c1[0]*c2[0] + c1[1]*c2[1])/divide
+    imag = (c2[0]*c1[1] - c1[0]*c2[1])/divide
+    return np.array([real,imag])
+  
+def product(c1,c2): #to multiply
+    real = c1[0]*c2[0] - c1[1]*c2[1])
+    imag = c2[0]*c1[1] + c1[0]*c2[1]
+    return np.array([real,imag])
 
